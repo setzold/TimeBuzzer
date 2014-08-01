23f12f02f2e53e494f5b13278af9d1bfba2bdda1
setzold <etzold.stefan@gmail.com>
2014-08-01 20:25:17 +0200

    added stuff test for timespan

---
+++ zold.TimeBuzzer.Business.Tests/StuffTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace zold.TimeBuzzer.Business.Tests
+{
+    /// <summary>
+    /// Class represents a container testing any stuff that is not assigned to existing business logic of the application.
+    /// </summary>
+    [TestClass]
+    public class StuffTests
+    {
+        [TestMethod]
+        public void Test_Some_Stuff()
+        {
+            //Test TimeSpan
+            TimeSpan time = new TimeSpan(12, 18, 02);
+
+            DateTime checkedDateTime = new DateTime(2014, 07, 01, 12, 18, 02);
+            Assert.AreEqual(checkedDateTime.TimeOfDay, time);
+            Assert.AreEqual(checkedDateTime.ToString("hh:mm:ss"), time.ToString());
+        }
+    }
+}
