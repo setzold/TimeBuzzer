5d2489e6ad52a917692dfc80221176f862adf3ac
setzold <etzold.stefan@gmail.com>
2014-08-04 22:10:48 +0200

    Add notify icon and create click and doube click events on it

--- zold.TimeBuzzer.Frontend/View/MainWindowView.xaml.cs
+++ zold.TimeBuzzer.Frontend/View/MainWindowView.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -11,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using zold.TimeBuzzer.Frontend.Tray;
 
 namespace zold.TimeBuzzer.Frontend.View
 {
@@ -19,9 +21,28 @@ namespace zold.TimeBuzzer.Frontend.View
     /// </summary>
     public partial class MainWindow : Window
     {
+        TrayIcon _tray;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            _tray = new TrayIcon(this, OnTrayMouseDoubleClick, OnTrayMouseClick);
+
+        }
+
+        private void OnTrayMouseClick()
+        {
+            MessageBox.Show("Tray clicked");
+        }
+
+
+        private void OnTrayMouseDoubleClick()
+        {
+            MessageBox.Show("Tray double clicked");
         }
     }
 }
