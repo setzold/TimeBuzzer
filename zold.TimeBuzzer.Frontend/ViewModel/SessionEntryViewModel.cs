384711e5690f3d93bca24c7ead8e89a35676a7ec
setzold <etzold.stefan@gmail.com>
2014-09-15 22:40:56 +0200

    add datepicker

--- zold.TimeBuzzer.Frontend/ViewModel/SessionEntryViewModel.cs
+++ zold.TimeBuzzer.Frontend/ViewModel/SessionEntryViewModel.cs
@@ -12,11 +12,6 @@ namespace zold.TimeBuzzer.Frontend.ViewModel
         private ISession _session;
 
         private string _error;
-        private string _editSessionDate;
-
-        private ICommand _editCommand;
-
-        private bool _isEditMode;
 
         private bool _sessionIsRunning;
 
@@ -24,8 +19,6 @@ namespace zold.TimeBuzzer.Frontend.ViewModel
         {
             _sessionIsRunning = true;
             _session = session;
-            _editSessionDate = _session.Date.ToString("dd.MM.yyyy");
-            _editCommand = new RelayCommand(OnEditCommand);
         }
 
         public double TotalHours
@@ -71,22 +64,6 @@ namespace zold.TimeBuzzer.Frontend.ViewModel
             }
         }
 
-        public string EditSessionDate
-        {
-            get { return _editSessionDate; }
-            set
-            {
-                _editSessionDate = value;
-
-                DateTime result;
-                if (DateTime.TryParse(_editSessionDate, out result))
-                    SessionDate = result;
-
-
-                RaiseOnPropertyChanged(() => EditSessionDate);
-            }
-        }
-
         public TimeSpan? EndTime
         {
             get { return _session.EndTime; }
@@ -96,27 +73,7 @@ namespace zold.TimeBuzzer.Frontend.ViewModel
                 RaiseOnPropertyChanged(() => EndTime);
             }
         }
-
-        public bool IsEditMode
-        {
-            get { return _isEditMode; }
-            set
-            {
-                _isEditMode = value;
-                RaiseOnPropertyChanged(() => IsEditMode);
-            }
-        }
-
-        public ICommand EditCommand
-        {
-            get { return _editCommand; }
-        }
-        
-        public string EditButtonToolTip
-        {
-            get { return "Change session date"; }
-        }
-
+                
         public void RefreshViewModel()
         {
             foreach (var property in this.GetType().GetProperties())
@@ -143,27 +100,11 @@ namespace zold.TimeBuzzer.Frontend.ViewModel
                         if (TotalHours.ToString().Length > 5)
                             _error = "Die Anzahl der Stunden erlaubt maximal 2 Stellen nach dem Komma.";
                         break;
-                    case "EditStartTime":
-                        DateTime dateResult;
-                        if (!string.IsNullOrWhiteSpace(_editSessionDate) && !DateTime.TryParse(_editSessionDate, out dateResult))
-                            _error = "Kein richtiges Datumsformat.";
-                        break;
 
                 }
 
                 return _error;
             }
-        }
-
-        private void OnEditCommand(object context)
-        {
-            if (_sessionIsRunning)
-                return;
-
-            IsEditMode = !_isEditMode;
-
-            if (_isEditMode)
-                EditSessionDate = _session.Date.ToString("dd.MM.yyyy");
         }
     }
 }
