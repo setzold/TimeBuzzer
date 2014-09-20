23515a0ea35cba808366674ee60fd246d35f5507
setzold <etzold.stefan@gmail.com>
2014-09-20 21:54:40 +0200

    adding sessions controller and tests

---
+++ zold.TimeBuzzer.Frontend.Tests/Controller/SessionRepositoryControllerTests.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using System.Linq;
+
+namespace zold.TimeBuzzer.Frontend.Controller.Tests
+{
+    [TestClass]
+    public class SessionRepositoryControllerTests
+    {
+        private static string TestFileName = "sessions.xml";
+        
+        [ClassInitialize]
+        public static void Initialize(TestContext ctx)
+        {
+            log4net.Config.XmlConfigurator.Configure();
+        }
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            if (File.Exists(TestFileName))
+                File.Delete(TestFileName);
+        }
+
+        [TestMethod]
+        public void Test_SessionRepositoryController_WithNoneExistingSessionsXml()
+        {
+            SessionRepositoryController testObject = new SessionRepositoryController();
+            testObject.Init();
+
+            Assert.AreEqual(0, testObject.Sessions.Count());
+        }
+
+        [TestMethod]
+        public void Test_SessionRepositoryController_WithExistingSessionsXml()
+        {
+            FileStream sessionsFile = File.Create(TestFileName);
+            sessionsFile.Close();
+            
+            SessionRepositoryController testObject = new SessionRepositoryController();
+            testObject.Init();
+
+            Assert.AreEqual(0, testObject.Sessions.Count());
+
+            File.Delete(TestFileName);
+        }
+
+        [TestMethod]
+        public void Test_SessionRepositoryController_Save()
+        {
+            SessionRepositoryController testObject = new SessionRepositoryController();
+            testObject.Save();
+            
+            bool fileExist = File.Exists(TestFileName);
+            Assert.IsTrue(fileExist);
+        }
+    }
+}
---
+++ zold.TimeBuzzer.Frontend/Controller/SessionRepositoryController.cs
@@ -0,0 +1,105 @@
+﻿using log4net;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using zold.TimeBuzzer.Business;
+using zold.TimeBuzzer.Interface;
+
+namespace zold.TimeBuzzer.Frontend.Controller
+{
+    public class SessionRepositoryController : IDisposable
+    {
+        private ILog _logger;
+        private static string _sessionsFileName = "sessions.xml";
+        private IList<ISession> _sessions;
+        private ISessionRepository _sessionRepository;
+        private bool _disposing;
+
+        public SessionRepositoryController()
+        {
+            _logger = LogManager.GetLogger(GetType());
+            _sessionRepository = new SessionsFileRepository(_sessionsFileName);
+            _sessions = new List<ISession>();
+        }
+
+        public IEnumerable<ISession> Sessions
+        {
+            get { return _sessions; }
+            private set { _sessions = value.ToList(); }
+        }
+
+        public void Init()
+        {
+            LogMethod(() =>
+                {
+                    if (!File.Exists(_sessionsFileName))
+                    {
+                        _logger.WarnFormat("Sessions file ({0}) doesn´t exist.", _sessionsFileName);
+                        return;
+                    }
+
+                    try
+                    {
+                        _sessions = _sessionRepository.LoadAllSessions().ToList();
+                    }
+                    catch (Exception ex)
+                    {
+                        _logger.Error("Error on Init.", ex);
+                    }
+
+                    int count = _sessions != null ? _sessions.Count : 0;
+                    _logger.DebugFormat("{0} sessions found");
+                });
+        }
+
+        public void Save()
+        {
+            LogMethod(() =>
+                   {
+                       _sessionRepository.SaveSessions(_sessions);
+
+                       if (_disposing)
+                       {
+                           _logger.Warn("Already disposed. No externa save processes are allowed.");
+                           return;
+                       }
+                           
+                       InternalSave();
+                   });
+        }
+
+        private void InternalSave()
+        {
+            LogMethod(() =>
+                {
+                    if (_sessions != null)
+                        _sessionRepository.SaveSessions(_sessions);
+                });
+        }
+
+        public void Dispose()
+        {
+            LogMethod(() =>
+               {
+                   _disposing = true;
+                   InternalSave();
+
+                   if (_sessions != null)
+                       _sessions.Clear();
+               });
+        }
+
+        private void LogMethod(Action action)
+        {
+            string methodName = new StackFrame(1).GetMethod().Name;
+            _logger.Debug(methodName + " - enter");
+
+            action();
+
+            _logger.Debug(methodName + " - leave");
+        }
+    }
+}
